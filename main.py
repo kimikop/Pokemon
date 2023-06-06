62f271963226bf09b75b5370bc0239b57ec3a7a1
Jhwang626 <133681625+Jhwang626@users.noreply.github.com>
2023-06-06 16:05:00 +0000

    Ted help accessing API/position of reader in file!

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import requests
 offset = 0
 endpoint = "https://pokeapi.co/api/v2/"
 poke_endpoint = endpoint + "pokemon/"
+offset_poke_endpoint_fun = lambda offset: poke_endpoint + "?offset={}&limit=10".format(offset)
+new_endpoint = offset_poke_endpoint_fun(offset)
 
 user_action = True
