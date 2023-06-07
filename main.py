2de39aa62f486e28932e7483670a1a9ce86d8b61
kimikop <133681933+kimikop@users.noreply.github.com>
2023-06-07 22:04:24 +0000

    Cleanup of next and back code >:)

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-print ("hey team")
 import urllib.request
 import json
 import requests
-
 offset = 0
+
 endpoint_without_offset = "https://pokeapi.co/api/v2/pokemon?limit=10&offset="
 user_action = True
 new_endpoint = endpoint_without_offset + str(offset)
 
+
 while user_action:
   response = requests.get(new_endpoint)
+  response.raise_for_status()
   data = response.json()
-  cuurent_poke = data['results']
+  current_poke = data['results']
   pokes = []
   for pokemon in current_poke:
     poke_name = pokemon['name']
     pokes.append(poke_name)
-    print("----------")
-    print(pokes)
-    user _action = input("\n\ntype: 'next' to see more pokemon opptions type: 'pokemon name' to learn more about specific pokemon, type: 'back' to see the previous list of pokemon. type 'stop' to end.\n\n:")
-    print("-----------")
-    if user_action == "stop":
-  user_action = False
-  break
+  print("-----------")
+  print(pokes)
+  user_action = input("\n\ntype: 'next' to see more pokemon options type: 'pokemon name' to learn more about specific pokemon, type: 'back' to see the previous list of pokemon. type 'stop' to end.\n\n:")
+  print("-----------")
+  if user_action == "stop":
+    user_action = False
+    break
 
-if user_action == "next":
-  offset += 10
-  new_endpoint = endpoint_without_offset + str(offset)
-
-elif user_action == "back":
-  if (offset > 0):
+  if user_action == "next":
     offset += 10
     new_endpoint = endpoint_without_offset + str(offset)
-  else: print("You cannot go any further back")
+
+  elif user_action == "back":
+    if (offset > 0):
+      offset -= 10
+      new_endpoint = endpoint_without_offset + str(offset)
+    else: print("You cannot go any further back")
     
