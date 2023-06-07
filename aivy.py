64e42e4d6f08858d6ab1edfb146e2afdeffd26d8
aivywa <111718865+aivywa@users.noreply.github.com>
2023-06-07 18:31:27 +0000

    if statements in personal file

--- aivy.py
+++ aivy.py
@@ -5,3 +5,16 @@ import requests
 offset = 0
 endpoint = "https://pokeapi.co/api/v2/"
 
+if user_action == "stop":
+  user_action = False
+  break
+
+if user_action == "next":
+  offset += 10
+  new_endpoint = endpoint_without_offset + str(offset)
+
+elif user_action == "back":
+  if (offset > 0):
+    offset += 10
+    new_endpoint = endpoint_without_offset + str(offset)
+  else: print("You cannot go any further back")
